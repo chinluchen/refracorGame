8aae273178436e9c3a44d1be0f350ebae68d9992
chinluchen <chinlu0322@gmail.com>
2025-03-25 12:32:56 +0800

    0325上傳網頁版本

--- script/cursor_manage.gd
+++ script/cursor_manage.gd
@@ -4,7 +4,7 @@ extends Node
 var hand = load("res://assets/images/hand.png")
 var handClick = load("res://assets/images/handClick.png")
 # 設定滑鼠熱點 (假設圖片大小為 32x32，將熱點設為中心)
-var hotspot = Vector2(53, 42)
+var hotspot = Vector2(53, 40)
 
 func _ready():
 	# 設定初始滑鼠指標
